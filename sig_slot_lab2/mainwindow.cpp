fbefb5f4b203a4f3ff991e03adf003f04300bd12
dimapichuev2000 <55379930+dimapichuev2000@users.noreply.github.com>
2020-04-17 02:25:38 +0900

    last vers

--- sig_slot_lab2/mainwindow.cpp
+++ sig_slot_lab2/mainwindow.cpp
@@ -23,6 +23,6 @@ void MainWindow::randNum()
 {
     int num = (qrand() % ((100 + 1) + 0) - 0);
     ui->label_2->setNum(num);
-    ui->progressBar->setValue(num);
+    ui->progressBar->setValue(num);//не смог преобразовать из label в progressBar,а с lcdNumber нашел как сделать через подсказки
 }
 
